47770f3c69abc8d2738e5773f1123a5a7e5b7d2e
qpyparty <rejzorkalahen@gmail.com>
2022-04-02 01:53:31 +0300

    неудачный фикс

--- src/functions/submit.py
+++ src/functions/submit.py
@@ -1,18 +1,24 @@
 from json import loads
 from tkinter import END
 
+from fake_useragent import UserAgent
+
 from src.config.static import s7
 from src.dataclasses import labels as lbl, entries as entr
 from src.tools.reduction import reduce
 
 
 def submit(labels: lbl.Labels, entries: entr.Entries) -> None:
-    response = s7.request(
-        entries.method.get(),
-        entries.url.get(),
-        headers=loads(entries.headers.get("1.0", END)),
+    params = dict(
+        method=entries.method.get(),
+        url=entries.url.get(),
         json=loads(entries.json.get("1.0", END)),
     )
+    headers = loads(entries.headers.get("1.0", END))
+    if headers:
+        response = s7.request(headers=headers, **params)
+    else:
+        response = s7.request(headers={"User-Agent": UserAgent().random}, **params)
 
     labels.result.configure(text=reduce(response.text))
     labels.status.configure(text=str(response.status_code))
